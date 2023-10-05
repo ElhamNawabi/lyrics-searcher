dafd18653102c11f7c1e2b78b716b36a7ac3cc56
ElhamNawabi <nawabi.elham@gmail.com>
2023-10-05 15:41:24 -0400

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Head from 'next/head';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Lyrics Finder</title>
+        <link rel="icon" href="/favicon.icon"></link>
+        <link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+					rel="stylesheet"
+				/>
+      </Head>
+    <Component {...pageProps} />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      background: "#000000",
+      primary: "#FFFFFF",
+      secondary: "#9E9E9E",
+      active: "#F40812",
+      light: "#121212",
     },
   },
+
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+//         'gradient-conic':
+//           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+
